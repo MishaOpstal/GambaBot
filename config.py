9fc13279a8fbe76894ef88d2a4fb6ab612dd1734
Misha Opstal <mishie.opstal@gmail.com>
2026-02-26 13:09:32 +0100

    Refactor Redis password handling in configuration and Docker Compose

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     REDIS_HOST = os.getenv("REDIS_HOST", "redis")
     REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
     REDIS_DB = int(os.getenv("REDIS_DB", 0))
-    REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
+    REDIS_PASSWORD = os.getenv("REDIS_PASSWORD") or None
 
     # Web Server
     DOMAIN = os.getenv("DOMAIN", "localhost")
